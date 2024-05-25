e057a158dfc424f3d5d0ed5afde573d1574bc832
kate <kate09030506@gmail.com>
2024-05-25 18:10:38 -0400

    api detail

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import PublicationList
-
+from .views import PublicationList, PublicationDetail
 urlpatterns = [
     path('', PublicationList.as_view(), name="publication_list"),
+    path('<int:pk>/', PublicationDetail.as_view(), name="publication_detail"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -7,3 +7,7 @@ from .serializers import PublicationSerializer
 class PublicationList(generics.ListCreateAPIView):
     queryset = Publication.objects.all()
     serializer_class = PublicationSerializer
+
+class PublicationDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Publication.objects.all()
+    serializer_class = PublicationSerializer
