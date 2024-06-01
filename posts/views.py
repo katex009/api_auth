bd0d9782ce0cda28951aae017a36a57dedb01bcb
kate <kate09030506@gmail.com>
2024-06-01 16:28:27 -0400

    permissions personalizados added

--- posts/views.py
+++ posts/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Publication
 from .serializers import PublicationSerializer
+from .permissions import IsAuthorOrReadOnly
 # Create your views here.
 
 class PublicationList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Publication.objects.all()
     serializer_class = PublicationSerializer
 
